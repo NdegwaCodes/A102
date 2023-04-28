8f59e4720eed941ad953e004a9ce82ab6ca18990
Ndegwacodes_ <50831913+NdegwaCodes@users.noreply.github.com>
2023-04-28 23:12:07 +0300

    Updates

---
+++ Real Estate Application/src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+
+
+function App() {
+  return (
+    <div>
+      <header className='p-4'>
+        <a href='' className='flex items-center gap-1'>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819" />
+</svg>
+<span className='font-bold text-xl'>airbnb</span>
+</a>
+<div>
+  <div>Anywhere</div>
+  <div>Any Week</div>
+  <div>Add Guests</div>
+  <button>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+</svg>
+
+  </button>
+</div>
+
+      </header>
+    </div>
+  )
+}
+
+export default App
